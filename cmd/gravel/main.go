3c1469368d52c72f3e2423099d5bb2be2393ef29
Eric Marshall <ericmarshall715@gmail.com>
2022-10-21 22:23:50 -0400

    Add description for binary

--- cmd/gravel/main.go
+++ cmd/gravel/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/alecthomas/kong"
 	"github.com/emm035/gravel/cmd/gravel/internal/cli"
 )
+
+var description = strings.Trim(`
+Gravel monorepo build tool
+`, " \n\t")
 
 var GravelCli struct {
 	Build   cli.BuildCmd   `name:"build" cmd:"" help:"Run a test/build cycle for any packages that have changed since the last run"`
@@ -11,6 +17,8 @@ var GravelCli struct {
 }
 
 func main() {
-	ktx := kong.Parse(&GravelCli)
+	ktx := kong.Parse(&GravelCli,
+		kong.Description(description),
+	)
 	ktx.FatalIfErrorf(ktx.Run())
 }
