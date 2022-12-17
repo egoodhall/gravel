8035c936f6c3dbfacf9d5a1e901312495cf3da49
Eric Marshall <ericmarshall715@gmail.com>
2022-12-16 22:50:53 -0500

    No default config files

--- cmd/gravel/main.go
+++ cmd/gravel/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	ktx := kong.Parse(new(cli.Cli),
 		kong.Description(cli.Description),
-		kong.Configuration(cli.YAML, "gravel.yml", "gravel.yaml"),
+		kong.Configuration(cli.YAML),
 	)
 	ktx.FatalIfErrorf(ktx.Run())
 }
